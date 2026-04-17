3f2661e31b5614153470d573ca9c688d9e0c98b6
Mingyang <kohmingyang@outlook.com>
2026-04-17 20:41:52 +0800

    fix: goose migration statement

--- mrbs-backend/migrations/00010_add_booking_colors.sql
+++ mrbs-backend/migrations/00010_add_booking_colors.sql
@@ -1,5 +1,10 @@
 -- +goose Up
+-- +goose StatementBegin
 ALTER TABLE mrbs.bookings ADD colour integer DEFAULT 1 NULL;
 
+-- +goose StatementEnd
+
 -- +goose Down
+-- +goose StatementBegin
 ALTER TABLE mrbs.bookings DROP COLUMN IF EXISTS colour;
+-- +goose StatementEnd
